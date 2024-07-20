bd904655e876895963371ef9d0e59b33ef74adac
YvanSolis <167389039+YvanSolis@users.noreply.github.com>
2024-07-20 23:40:05 +0800

    Add files via upload

--- mainmenu.js
+++ mainmenu.js
@@ -1,13 +1,22 @@
+// Initialize Swiper (for Page 1)
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
     spaceBetween: 30,
-    slidesPerGroup: 1,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
     },
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// Initialize Bootstrap Carousel (for Page 2)
+$('#carouselExampleIndicators').carousel({
+    interval: 2000,
 });
